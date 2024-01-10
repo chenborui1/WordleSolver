3452cf38e562830802b3cccaf84c15d58ed91a36
chenborui1 <150975091+chenborui1@users.noreply.github.com>
2024-01-10 14:35:33 -0500

    ss

--- wordleStrategy.py
+++ wordleStrategy.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 current_directory = os.getcwd()
 
@@ -12,7 +13,35 @@ words_to_list = words.split("\n")
 
 file.close()
 
-print(words_to_list)
+# print(words_to_list)
+
+
+
+def find_first_guess(wordList):
+    emptyList = []
+    for word in wordList:
+        counter = 0
+        letters = set(word)
+        for x in 'aeiou':
+            if x in letters:
+                counter += 1
+        if counter == 4:
+            emptyList.append(word)
+            
+    return emptyList
+
+
+listOfMostUniqueVowels = find_first_guess(words_to_list)
+random_most_unique_vowel_word = random.choice(listOfMostUniqueVowels)
+
+print(random_most_unique_vowel_word)
+    
+
+
+
+
+
+
 
 
 
