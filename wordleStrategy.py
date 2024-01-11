78af82e427d8330d651331a92e38ab42ac52d2fb
chenborui123 <chenborui123@outlook.com>
2024-01-10 20:31:52 -0500

    commit

--- wordleStrategy.py
+++ wordleStrategy.py
@@ -1,21 +1,16 @@
 import os
 import random
+import re
 
+#Initialize word list
 current_directory = os.getcwd()
-
-
-
 file=open("words.txt", "r")
-
 words = file.read()
-
 words_to_list = words.split("\n")
-
 file.close()
 
-# print(words_to_list)
 
-
+#Methods
 
 def find_first_guess(wordList):
     emptyList = []
@@ -30,11 +25,55 @@ def find_first_guess(wordList):
             
     return emptyList
 
+def validResult(result):
+    checkerForInteger = False
+    if len(result) != 5:
+        return False
+    if not bool(re.match('^[012]+$', result)):
+        return False
+    return True
 
+
+def inputResult():
+    while True:
+    
+        result = input('Result? ')
+        if validResult(result):
+            return result
+        else:
+            print("Invalid result")
+
+def analyze_result(wordUsed, userInput, availableAlphabets):
+    #Need to update word alphabets based on user input
+    #Analyze user input and word used to change availableAlphabets
+    #Ultimately get the next best word to try
+    pass       
+
+
+#Code base
+    
 listOfMostUniqueVowels = find_first_guess(words_to_list)
 random_most_unique_vowel_word = random.choice(listOfMostUniqueVowels)
 
-print(random_most_unique_vowel_word)
+alphabetsToUse = ['abcdefghijklmnopqrstuvwxyz']
+firstWord = random_most_unique_vowel_word
+
+print("Put in: " + random_most_unique_vowel_word)
+
+# Input 5 digit number that consists of only 0, 1, or 2
+# Where 0 is that letter is not present
+# Where 1 is that letter is present but not at the correct index
+# Where 2 is that is at the correct spot
+
+analyze_result(firstWord, inputResult(), alphabetsToUse)
+
+
+
+
+
+    
+
+
     
 
 
