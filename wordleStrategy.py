c1d3caf5364ef2b28da6ac90a0812e58fd6250ca
chenborui1 <150975091+chenborui1@users.noreply.github.com>
2024-01-10 22:00:15 -0500

    fgg

--- wordleStrategy.py
+++ wordleStrategy.py
@@ -25,6 +25,13 @@ def find_first_guess(wordList):
             
     return emptyList
 
+def filterWordsByLetter(wordList, alphabet):
+    for word in wordList:
+        letters = set(word)
+        if alphabet in letters:
+            wordList.remove(letters)
+    return wordList
+
 def validResult(result):
     checkerForInteger = False
     if len(result) != 5:
@@ -43,7 +50,7 @@ def inputResult():
         else:
             print("Invalid result")
 
-def analyze_result(wordUsed, userInput, availableAlphabets):
+def analyze_result(wordUsed, userInput, availableAlphabets, wordList):
     
     #Need to update word alphabets based on user input
     #Analyze user input and word used to change availableAlphabets
@@ -52,14 +59,20 @@ def analyze_result(wordUsed, userInput, availableAlphabets):
     #First reduce word list length by getting rid of all alphabets that are not present
     index = 0
     for element in userInput:
+
+        # Input 5 digit number that consists of only 0, 1, or 2
+        # Where 0 is that letter is not present
+        # Where 1 is that letter is present but not at the correct index
+        # Where 2 is that is at the correct spot
+
         if element is '0':
             availableAlphabets = availableAlphabets.replace(wordUsed[index], '')
-        if element is '1':
-            availableAlphabets = availableAlphabets.replace(wordUsed[index], '')
-        if element is '2':
-            availableAlphabets = availableAlphabets.replace(wordUsed[index], '')
+            filterWordsByLetter(wordList, wordUsed[index])
+       
         index += 1
-    print(availableAlphabets)
+    
+    
+    print(wordList)
             
 
 
@@ -76,12 +89,9 @@ firstWord = random_most_unique_vowel_word
 
 print("Put in: " + random_most_unique_vowel_word)
 
-# Input 5 digit number that consists of only 0, 1, or 2
-# Where 0 is that letter is not present
-# Where 1 is that letter is present but not at the correct index
-# Where 2 is that is at the correct spot
 
-analyze_result(firstWord, inputResult(), alphabetsToUse)
+
+analyze_result(firstWord, inputResult(), alphabetsToUse, words_to_list)
 
 
 
